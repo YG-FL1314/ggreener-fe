d6daf6c229eae355114f036506032b09d774c45c
lifu <lifu@staff.sina.com.cn>
2018-10-19 16:58:33 +0800

    bugfix

--- js/ggreen.js
+++ js/ggreen.js
@@ -1,47 +1,68 @@
-function memberList() {
-   url = "/tag/list?parentId=1"
-   $.ajax({    
-        type:'get',  
-        xhrFields: {
-            withCredentials: true
-        },   
-        url: url,    
-        cache:true,   
-        crossDomain: true,
-        dataType:'json',    
-        success:function(data){
-            console.log(data)
-        },
-        error:function(data){
-            $.messager.show({
-                title:'登录失败',
-                msg: data,
-                showType:'fade',
-                style:{
-                    left:0,
-                    right:'',
-                }
-            });
-        }  
-    }); 
+/*全局变量*/
+const MEMBER_FLAG = 1
+const COMPANY_FLAG = 8
+const REGION_FLAG = 20
+const ATTENTION_FLAG = 16
+const ZOL_FLAG = 55
+
+
+function getTags(parentId) {
+    var result;
+    $.ajax({
+        url: "/tag/list?parentId=" + parentId,
+        dataType: 'json',
+        async: false,
+        success: function(data){
+            var items = $.each(data.obj,function(idx,item){ 
+                return {
+                    id: item.id,
+                    name: item.name
+                };
+            })
+            result = items
+        },
+        error: function(){
+            error.apply(this, arguments);
+        }
+    });
+    return result;
 }
-var myloader = function(param,success,error){
-        $.ajax({
-            url: "/tag/list?parentId=1",
-            dataType: 'json',
-            success: function(data){
-                var items = $.each(data.obj,function(idx,item){ 
-                    return {
-                        id: item.id,
-                        name: item.name
-                    };
-                })
-                success(items)
-            },
-            error: function(){
-                error.apply(this, arguments);
-            }
-        });
-    }
+/*页面加载*/ 
 window.onload = function () { 
+    $('#member').combobox({
+        valueField: 'id', 
+        textField: 'name',
+        panelHeight:'auto',
+        limitToList: '6',
+        data: getTags(MEMBER_FLAG)
+    });
+
+    $('#companyTypes').combobox({
+        valueField: 'id', 
+        textField: 'name',
+        panelHeight:'auto', 
+        limitToList: '6',
+        data: getTags(COMPANY_FLAG)
+    });
+    $('#region').combobox({
+        valueField: 'id', 
+        textField: 'name',
+        panelHeight:'auto', 
+        limitToList: true,
+        data: getTags(REGION_FLAG)
+    });
+    $('#attention').combobox({
+        valueField: 'id', 
+        textField: 'name',
+        panelHeight:'auto', 
+        limitToList: 'true',
+        data: getTags(ATTENTION_FLAG)
+    });
+    $('#zol').combobox({
+        valueField: 'id', 
+        textField: 'name',
+        panelHeight:'auto', 
+        limitToList: '6',
+        data: getTags(ZOL_FLAG)
+    });
 }
